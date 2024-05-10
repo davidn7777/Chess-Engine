a38d8cc0ddf25c6515eca0d4d058920d753f847f
davidn7777 <david.neilan@gmail.com>
2024-05-10 13:23:03 +0100

    Added live web board

--- ChatChess/app.py
+++ ChatChess/app.py
@@ -6,36 +6,41 @@ def load_svg(svg_path):
     with open(svg_path, "r") as file:
         return file.read()
 
-
-def main():
+def app():
     col1,col2 = st.columns(2)
+
+    if 'fen' not in st.session_state:
+        st.session_state.fen = ChessGame.gen_board()  
+
+    if 'svg_data' not in st.session_state:
+        svg_file = 'board.svg'
+        st.session_state.svg_data = load_svg(svg_file)
+
     with col1:
-        with st.container():
-            if 'fen' not in st.session_state:
-                st.session_state.fen = ChessGame.gen_board()  # Initial value      
-            
-            # Set the title of the app
+        with st.container():  
             st.title('Chat Chess')
-            
-            # User input for UCI move
             uci_move = st.text_input('Enter your UCI move')
 
 
     with col2:
         with st.container():
-            # Button to submit move
+            if 'runtime' not in st.session_state:
+                st.session_state.runtime = True
+                svg_file = 'board.svg'
+                st.session_state.svg_data = load_svg(svg_file)
+
             if st.button('Submit Move'):
                 st.write(f'You entered the move: {uci_move}')
                 if uci_move in ChessGame.get_legal(st.session_state.fen):
                     st.session_state.fen = ChessGame.move(st.session_state.fen,uci_move)
+                    svg_file = 'board.svg'
+                    st.session_state.svg_data = load_svg(svg_file)
                 else:
                     st.error("Please input a legal move in the form: e2e4")
 
-                svg_file = 'board.svg'  # Update the path to your SVG file
-                svg_data = load_svg(svg_file)
-                st.markdown(f'<div style="text-align: left;">{svg_data}</div>', unsafe_allow_html=True)
+            st.markdown(f'<div style="text-align: left;">{st.session_state.svg_data}</div>', unsafe_allow_html=True)
 
 if __name__ == "__main__":
     st.set_page_config(layout="wide")
-    main()
+    app()
 
